c6f1b52cc71f91398514bcb60b5654b7dd3eeeea
openthread <openfibers@gmail.com>
2013-05-24 14:20:57 +0800

    Finished new demo

--- LoadProgressWebViewDemo/LoadProgressWebViewDemo/ViewController.m
+++ LoadProgressWebViewDemo/LoadProgressWebViewDemo/ViewController.m
@@ -7,23 +7,36 @@
 //
 
 #import "ViewController.h"
+#import "LoadProgressWebView.h"
 
-@interface ViewController ()
+@interface ViewController () <WebResourceLoadProgressDelegate>
 
 @end
 
 @implementation ViewController
+{
+    LoadProgressWebView *_progressWebView;
+    UILabel *_infoLabel;
+}
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-	// Do any additional setup after loading the view, typically from a nib.
+    _progressWebView = [[LoadProgressWebView alloc] initWithFrame:(CGRect){CGPointZero, self.view.frame.size}];
+    _progressWebView.progressDelegate = self;
+    [self.view addSubview:_progressWebView];
+    
+    NSURLRequest *req = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:@"http://baidu.com/"]];
+    [_progressWebView loadRequest:req];
+    
+    _infoLabel = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, 100, 44)];
+    _infoLabel.backgroundColor = [UIColor clearColor];
+    [self.view addSubview:_infoLabel];
 }
 
-- (void)didReceiveMemoryWarning
+- (void)webView:(LoadProgressWebView *)webView didReceiveResourceNumber:(NSInteger)resourceNumber totalResources:(NSInteger)totalResources
 {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+    _infoLabel.text = [NSString stringWithFormat:@"%d/%d", resourceNumber, totalResources];
 }
 
 @end
